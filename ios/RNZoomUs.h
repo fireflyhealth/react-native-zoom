c3defa049493e2718f22654939982fe8ab996999
Ian Ebstein <ian@firefly.health>
2020-04-02 16:44:44 -0400

    Out with the old

--- ios/RNZoomUs.h
+++
@@ -1,8 +0,0 @@
-
-#import "RCTBridgeModule.h"
-#import <MobileRTC/MobileRTC.h>
-
-@interface RNZoomUs : NSObject <RCTBridgeModule, MobileRTCAuthDelegate, MobileRTCMeetingServiceDelegate>
-
-@end
-  
